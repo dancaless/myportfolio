b8aae982a5312d4851440b3f4060c3fdc421d462
unknown <you@example.com>
2021-07-09 22:56:36 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import Header from '../header/header'
+import About from '../about/about'
+import Portfolio from '../portfolio/portfolio'
+import Contact from '../contact/contact'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/portfolio/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Portfolio extends React.Component {
+
+    render() {
+        return (
+            <section className='portfolio'>
+                мое портфолио
+            </section>
+        )
+    }
+};
